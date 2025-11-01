629cd5440a76a437cb0f22890a757b8c05c0319b
DDH-WEB <https://github.com/DDH-WEB>
2025-11-01 09:47:49 -0500

    Deply FrontEnd rasposive 2

--- src/view/EditProduct.tsx
+++ src/view/EditProduct.tsx
@@ -45,7 +45,13 @@ export default function EditProduct() {
         {error && <ErrorSms key={Date.now()}>{error}</ErrorSms>}
         <div className='flex justify-between'>
             <h2 className='text-4xl font-black text-slate-500'>Editar Producto</h2>
-            <Link className='rounded-md text-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500' to='/'>Ver Productos</Link>
+            <Link
+              to="/"
+              className="flex items-center justify-center gap-2 p-3 text-sm font-bold text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 transition-colors duration-200"
+            >
+              <span className="flex justify-center items-center text-lg font-bold sm:hidden rounded-full h-3 w-3">👁‍🗨</span>
+              <span className="hidden sm:inline">👁‍🗨 Ver Productos</span>
+            </Link>
         </div>
         <Form
             className="mt-10"      
